702963c6560941b571efba96caa87b0eab02a596
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-11-03 15:28:52 +0100

    load movie json

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,25 @@
 import React from "react";
+import moviesArr from "../data/movies.json"
+
+
 
 function Main() {
-    return <p>This is the main content</p>
+   
+    return (
+        <>
+            {moviesArr.map( (movie, index) => {
+                return (
+                <div className= "movie-container" key={movie.id}>
+                    <h2>{movie.title}</h2>
+                    <p>Year: {movie.year}</p>
+                    Rating: {movie.rating}
+                </div>
+                
+                )
+
+            })}
+        </>
+    );
 }
 
 
